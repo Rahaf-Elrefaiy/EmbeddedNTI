5dc36b6138a644c6a9bb12b6e2fad9d632aa1d34
Rahaf-Elrefaiy <rahafmohamed767@gmail.com>
2026-09-03 15:20:31 +0300

    Add files via upload

---
+++ Embedded/Headers/Bit_Math.h
@@ -0,0 +1,20 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 9/2/2026 10:43:42 PM
+ *  Author: Admin
+ */ 
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+#define SET_BIT(REG, POS)     ((REG) |= (1 << (POS)))
+#define CLR_BIT(REG, POS)     ((REG) &= ~(1 << (POS)))
+#define TOG_BIT(REG, POS)     ((REG) ^= (1 << (POS)))
+#define GET_BIT(REG, POS)     (((REG) >> (POS)) & 1)
+
+#endif
+
+
+
+
