e840132726f4cd3f5ad04badd20912e6a80a909b
Rahaf-Elrefaiy <rahafmohamed767@gmail.com>
2026-09-07 23:48:15 +0300

    Add files via upload

---
+++ Embedded/labs/Lab4_ADC/main.c
@@ -0,0 +1,38 @@
+/*
+ * GccApplication16.c
+ *
+ * Created: 9/7/2026 9:19:36 PM
+ * Author : Admin
+ */ 
+
+#include "Std_types.h"
+#include "Bit_Math.h"
+#include "DIO.h"
+#include "ADC.h"
+
+#define LDR_THRESHOLD   500
+
+int main(void)
+{
+	u16 adc_value = 0;
+
+	ADC_Init();
+	DIO_voidSetPinDirection(DIO_PORTC, PIN0, OUTPUT); 
+
+	while(1)
+	{
+		adc_value = ADC_Read(0);
+
+		if(adc_value < LDR_THRESHOLD)
+		{
+			// Dark: Turn LED ON
+			DIO_voidSetPinValue(DIO_PORTC, PIN0, HIGH);
+		}
+		else
+		{
+			// Bright: Turn LED OFF
+			DIO_voidSetPinValue(DIO_PORTC, PIN0, LOW);
+		}
+	}
+	return 0;
+}
